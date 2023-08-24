0d50df1c6c1ad4c89b1d5909167bdf8825dbe1ef
Griffin Milsap <griffin.milsap@jhuapl.edu>
2023-08-24 16:42:45 -0400

    added tap mode

--- ezmsg/hid/keyboard.py
+++ ezmsg/hid/keyboard.py
@@ -8,12 +8,13 @@ import ezmsg.core as ez
 class KeyboardMessage:
     control_keys: chr = 0x00
     hid_keycode: chr = 0x00
+    tap: bool = True
 
     def report(self) -> bytearray:
-        buf = [0] * 8
+        buf = [0] * 16
         buf[0] = self.control_keys
         buf[2] = self.hid_keycode
-        return bytearray(buf)
+        return bytearray(buf[:(16 if self.tap else 8)])
     
 class KeyboardDeviceSettings(ez.Settings):
     keyboard_path: str = '/dev/hidg0'
@@ -28,16 +29,3 @@ class KeyboardDevice(ez.Unit):
             self.SETTINGS.keyboard_path, 
             msg.report()
         )
-
-        # If it's a normal keycode (i.e. not a standalone modifier key), add a
-        # message indicating that the key should be released after it is sent. We do
-        # this to prevent the keystroke from incorrectly repeating on the target
-        # machine if network latency causes a delay between the keydown and keyup
-        # events. However, auto-releasing has the disadvantage of preventing
-        # genuinely long key presses (see
-        # https://github.com/tiny-pilot/tinypilot/issues/1093).
-        if msg.hid_keycode:
-            hid_write.write_to_hid_interface(
-                self.SETTINGS.keyboard_path, 
-                KeyboardMessage().report()
-            )
