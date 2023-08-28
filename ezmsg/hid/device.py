6b703e2789ed0d3d77287eb612223c9916fed7c7
Griffin Milsap <griffin.milsap@jhuapl.edu>
2023-08-28 15:05:11 -0400

    maybe async file writes?

--- ezmsg/hid/device.py
+++ ezmsg/hid/device.py
@@ -109,8 +109,7 @@ class HIDDeviceSettings(ez.Settings):
     device: Path
 
 class HIDDeviceState(ez.State):
-    # handle: BinaryFileWrapper
-    ...
+    handle: BinaryFileWrapper
 
 class HIDDevice(ez.Unit):
 
@@ -120,15 +119,12 @@ class HIDDevice(ez.Unit):
     INPUT_HID = ez.InputStream(ReportMessage)
 
     async def initialize(self) -> None:
-        # self.STATE.handle = async_open(self.SETTINGS.device, 'rb+')
-        ...
+        self.STATE.handle = async_open(self.SETTINGS.device, 'rb+')
 
     @ez.subscriber(INPUT_HID)
     async def write(self, msg: ReportMessage) -> None:
-        # await self.STATE.handle.write(buf)
-        write_to_hid_interface(self.SETTINGS.device, msg.report())
+        await self.STATE.handle.write(msg.report())
 
     async def shutdown(self) -> None:
-        # await self.STATE.handle.close()
-        ...
+        await self.STATE.handle.close()
         
