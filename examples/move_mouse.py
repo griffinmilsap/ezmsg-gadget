e1d50db7d47cf294d3c624f88ab607dd39cd3867
Griffin Milsap <griffin.milsap@jhuapl.edu>
2023-08-31 12:53:33 -0400

    sync changes on remote

--- examples/move_mouse.py
+++ examples/move_mouse.py
@@ -5,6 +5,7 @@ import time
 
 from pathlib import Path
 
+import numpy as np
 import ezmsg.core as ez
 
 from ezmsg.gadget.hiddevice import HIDDevice, HIDDeviceSettings
@@ -20,14 +21,20 @@ class MouseMessageGenerator(ez.Unit):
 
     @ez.publisher(OUTPUT)
     async def output_mouse(self) -> typing.AsyncGenerator:
+        start = time.time()
         while True:
+
+            if time.time() - start > 2.0:
+                raise ez.NormalTermination
+
             await asyncio.sleep(1.0 / self.SETTINGS.pub_rate)
 
-            w = 2.0 * math.pi * time.time()
-            cpx = math.exp(w * 1.0j) * (math.cos(0.1 * w) + 1.0) / 2.0
+            w = 2.0 * math.pi * 2.0 * time.time()
+            mag = (np.cos(0.5 * w) + 1.0) / 2.0
+            cpx = np.exp(w * 1.0j) * mag
             yield self.OUTPUT, MouseMessage(
-                relative_x = (cpx.real / 2.0) + 1.0,
-                relative_y = (cpx.imag / 2.0) + 1.0
+                relative_x = (cpx.real + 1.0) / 2.0,
+                relative_y = (cpx.imag + 1.0) / 2.0
             )
 
 if __name__ == '__main__':
