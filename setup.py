95beee6b7b474231ec3558ac6f6a7b30a0ddc99f
Griffin Milsap <griffin.milsap@jhuapl.edu>
2024-01-01 13:51:39 -0500

    removed old setup file

--- setup.py
+++
@@ -1,8 +0,0 @@
-import setuptools
-
-setuptools.setup(
-    name='ezmsg-gadget',
-    packages=setuptools.find_namespace_packages(include=['ezmsg.*']),
-    zip_safe=False,
-    include_package_data=True
-)
